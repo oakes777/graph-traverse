db9429833eba5f7074557c490a14d29af627bed8
Ben Mojo <155706745+oakes777@users.noreply.github.com>
2025-03-06 14:36:51 -0800

    JS initial commit

--- src/Traverse.java
+++ src/Traverse.java
@@ -1,14 +1,11 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Traverse {
   public static void main(String[] args) {
     // See below site for visualization of this graph
     // https://auberonedu.github.io/graph-explore/graph_site/viz.html
-    Vertex<Integer> v3  = new Vertex<>(3);
-    Vertex<Integer> v7  = new Vertex<>(7);
+    Vertex<Integer> v3 = new Vertex<>(3);
+    Vertex<Integer> v7 = new Vertex<>(7);
     Vertex<Integer> v12 = new Vertex<>(12);
     Vertex<Integer> v34 = new Vertex<>(34);
     Vertex<Integer> v56 = new Vertex<>(56);
@@ -18,16 +15,45 @@ public class Traverse {
     Vertex<Integer> v23 = new Vertex<>(23);
     Vertex<Integer> v67 = new Vertex<>(67);
 
-    v3.neighbors  = new ArrayList<>(List.of(v7, v34));
-    v7.neighbors  = new ArrayList<>(List.of(v12, v45, v34, v56));
-    v12.neighbors = new ArrayList<>(List.of(v7, v56, v78));
-    v34.neighbors = new ArrayList<>(List.of(v34, v91)); 
-    v56.neighbors = new ArrayList<>(List.of(v78));
-    v78.neighbors = new ArrayList<>(List.of(v91));
-    v91.neighbors = new ArrayList<>(List.of(v56));
-    v45.neighbors = new ArrayList<>(List.of(v23));
-    v23.neighbors = new ArrayList<>(List.of());
-    v67.neighbors = new ArrayList<>(List.of(v91));
+    v3.neighbors.addAll(List.of(v7, v34));
+    v7.neighbors.addAll(List.of(v12, v45, v34, v56));
+    v12.neighbors.addAll(List.of(v7, v56, v78));
+    v34.neighbors.addAll(List.of(v34, v91));
+    v56.neighbors.addAll(List.of(v78));
+    v78.neighbors.addAll(List.of(v91));
+    v91.neighbors.addAll(List.of(v56));
+    v45.neighbors.addAll(List.of(v23));
+    v23.neighbors.addAll(List.of());
+    v67.neighbors.addAll(List.of(v91));
+
+    //don't need this instantiation due to method overloading
+    // Set<Vertex<Integer>> visited = new HashSet<>();
+    dfs(v7);
   }
 
+  //method overloading
+  public static <T> void dfs(Vertex<T> vertex) {
+    //make tracker to avoid cycling
+    //call method overload buddy with two parameters
+    dfs(vertex, new HashSet<Vertex<T>>());
+
+  }
+
+  //if changed to private static method then can't bring in
+  //outside visited List! Can only be used inside this class
+  public static <T> void dfs(Vertex<T> vertex, Set<Vertex<T>> visited) {
+    //return if vertex null OR if visited already contains 
+    //current vertex
+    if (vertex == null || visited.contains(vertex)) return;
+
+    System.out.println(vertex.data);
+    //update visited with current vertex
+    visited.add(vertex);
+
+    if (vertex.neighbors == null) return;
+
+    for (Vertex<T> neighbor : vertex.neighbors) {
+      dfs(neighbor, visited);
+    }
+  }
 }
